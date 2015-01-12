64804cd09f1062c46c1644fb5a5921bbdca89eff
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-01-11 22:14:10 -0800

    Move Types from Points- to Codes-based

--- src/Types.v
+++ src/Types.v
@@ -1,94 +1,101 @@
 (** * Types as closures *)
 
-Require Import ObAxioms.
-Require Import Lambda.
-Require Import ConstructorOld. (* TODO migrate to new Constructor.v *)
-
-Open Scope Ob_scope.
+Require Export Constructor.
+Open Scope code_scope.
 
 (** ** Properties of types *)
 
-Definition closure (a : Ob) := I [= a /\ a o a = a.
-Definition fixes (a : Ob) (x : Ob) := a*x = x.
-Notation "x :: a" := (fixes a x) : Ob_scope.
+Definition closure {Var : Set} (a : Code Var) := I [= a /\ a o a = a.
+Definition fixes {Var : Set} (a : Code Var) (x : Code Var) := a*x = x.
+Notation "x :: a" := (fixes a x) : code_scope.
 
 (** ** Atomic types *)
 
-Lemma I_closure : closure I.
+Lemma I_closure {Var : Set} : closure (I : Code Var).
 Proof.
   unfold closure; split.
-  apply LESS_refl.
+  apply code_le_refl.
+  (* TODO adapt eta, beta tactics from Points to Codes.
   eta_expand; beta_reduce; auto.
-Qed.
+  *)
+Admitted.
 
-Theorem I_inhab (x : Ob) : x :: I <-> True.
+Theorem I_inhab (Var : Set) (x : Code Var) : x :: I <-> True.
 Proof.
   unfold fixes.
+  (* TODO adapt eta, beta tactics from Points to Codes.
   beta_reduce; firstorder.
-Qed.
+  *)
+Admitted.
 
-Lemma V_closure : closure V.
+Lemma V_closure (Var : Set) : closure (V : Code Var).
 Proof.
   unfold closure.
   split.
+  (* TODO adapt Y_fix, eta, beta tactics from Points to Codes.
     unfold V; rewrite Y_fix; rewrite Y_fix; fold V.
     freeze V in (compute; eta_expand as f; eta_expand as x; beta_reduce).
     apply LESS_trans with (x || f * x); monotonicity; auto.
-  (* TODO *)
+  TODO
+  *)
 Admitted.
 
-Theorem V_inhab (x : Ob) : x :: V <-> closure x.
+Theorem V_inhab (Var : Set) (x : Code Var) : x :: V <-> closure x.
 Proof.
-  unfold fixes. unfold closure.
+  unfold fixes; unfold closure.
   split.
     intros Hfix; rewrite <- Hfix; clear Hfix.
     split.
   (* TODO *)
 Admitted.
 
-Corollary fixes_V_V : fixes V V.
+Corollary fixes_V_V (Var : Set) : fixes V (V : Code Var).
 Proof.
   apply V_inhab.
   apply V_closure.
 Qed.
 
 Section semi.
-  Let a := VAR 0.
-  Let a' := VAR 1.
-  Definition semi := encode (\\a,a'; a --> a').
+  Variable Var : Set.
+  Let a := make_var Var 0.
+  Let a' := make_var Var 1.
+  Definition Semi := close (\\a,a'; a --> a').
 End semi.
+Definition semi {Var : Set} := Semi Var.
 
-Lemma semi_closure : closure semi.
+Lemma semi_closure (Var : Set) : closure (semi : Code Var).
 Proof.
   (* TODO *)
 Admitted.
 
-Theorem semi_inhab (x : Ob) : x :: semi <->
-  x = BOT \/
-  x = I \/
-  x = TOP.
+Theorem semi_inhab (Var : Set) (x : Code Var) : x :: semi <->
+  x [=] BOT \/
+  x [=] I \/
+  x [=] TOP.
 Proof.
   split.
   (* TODO *)
 Admitted.
 
 Section boool.
-  Let a := VAR 0.
-  Let a' := VAR 1.
-  Definition boool := encode (\\a,a'; a --> a --> a').
+  Variable Var : Set.
+  Let a := make_var Var 0.
+  Let a' := make_var Var 1.
+  Definition Boool := close (\\a,a'; a --> a --> a').
 End boool.
+Definition boool {Var : Set} := Boool Var.
 
-Lemma boool_closure : closure semi.
+Lemma boool_closure (Var : Set) : closure (boool : Code Var).
 Proof.
   (* TODO *)
 Admitted.
 
-Theorem boool_inhab (x : Ob) : x :: boool <->
-  x = BOT \/
-  x = K \/
-  x = F \/
-  x = J \/
-  x = TOP.
+Theorem boool_inhab (Var : Set) (x : Code Var) : x :: boool <->
+  x [=] BOT \/
+  x [=] K \/
+  x [=] K * I \/
+  x [=] J \/
+  x [=] TOP.
 Proof.
   split.
   (* TODO *)
