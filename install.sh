1e1ca14495d9b2081bcd91cbe186fd3ed865459b
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-01-14 08:18:21 -0800

    Use Stéphane Glondu ppa on travis

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@
 #  || wget http://isabelle.in.tum.de/dist/$HOL \
 #  && tar -xzf $HOL
 
+# latest version
 COQ=coq-8.4pl5.tar.gz
 test -e $COQ \
   || wget http://coq.inria.fr/distrib/V8.4pl5/files/$COQ \
+  && tar -xzf $COQ
+
+# version supported on travis
+COQ=coq-8.3pl5.tar.gz
+test -e $COQ \
+  || wget http://coq.inria.fr/distrib/V8.3pl5/files/$COQ \
   && tar -xzf $COQ
