e3a5828182e218e44e2d8ba53a60365ffe794762
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2015-02-06 17:15:04 -0800

    More work on bohm trees

--- sandbox/BohmTrees.v
+++ sandbox/BohmTrees.v
@@ -27,6 +27,9 @@ Fixpoint is_hnf {Var : Set} (x : Code Var) : bool :=
     | _ => false
   end.
 
+(* This gross combinatory definition requires side calculations.
+   Is there a more direct way to define hnf in terms of abstraction? *)
+
 Inductive hnf {Var : Set} : nat -> Code Var -> Prop :=
   | hnf_succ n x : hnf (Succ n) x -> hnf n x
   | hnf_var n v : hnf n (code_var v)
@@ -35,10 +38,23 @@ Inductive hnf {Var : Set} : nat -> Code Var -> Prop :=
   | hnf_k : hnf 0 K
   | hnf_k1 x : hnf 0 x -> hnf 0 (K * x)
   | hnf_s : hnf 0 S
-  | hnf_s1 x : hnf 2 x -> hnf 0 (S * x)
-  | hnf_s2 x y : hnf 2 x -> hnf 0 (S * x * y)
-  | hnf_j n x y : hnf n x -> hnf n y -> hnf n (x || y)
-  | hnf_r n x y : hnf n x -> hnf n y -> hnf n (x (+) y)
+  | hnf_s1 n x : hnf (Succ (Succ n)) x -> hnf n (S * x)
+  | hnf_s2 n x y : hnf (Succ (Succ n)) x -> hnf n (S * x * y)
+  | hnf_j2 n x y : hnf n x -> hnf n y -> hnf n (J * x * y)
+  | hnf_r2 n x y : hnf n x -> hnf n y -> hnf n (R * x * y)
+.
+
+(* Maybe try an abstraction-based definition
+Inductive hnf' {Var : Set} : Code Var -> Prop :=
+  hnf'_var v xs : hnf' (code_var v ** xs)
+  hnf'_app :
+*)
+
+(* or an order-based definition *)
+Inductive hnf'' {Var : Set} : Code Var -> Prop :=
+  | hnf''_var v : hnf'' (code_var v)
+  | hnf''_i : hnf'' I
+  | hnf''_k x : hnf'' x -> hnf'' (K * x)
 .
 
 Fixpoint approximate {Var : Set} (x : Code Var) : Code Var :=
@@ -50,6 +66,11 @@ Fixpoint approximate {Var : Set} (x : Code Var) : Code Var :=
 
 Inductive approximated_by {Var : Set} (x : Code Var) : Code Var -> Prop :=
   approximated_by_intro y : pi x y -> approximated_by x (approximate y).
+
+Theorem normal_conv (Var : Set) (c x : Code Var) :
+  conv (c * x) -> exists y, approximated_by x y /\ conv (c * y).
+Proof.
+Admitted.
 
 Theorem normal_interpolate {Var : Set} (x y : Code Var) :
   ~ y [= x -> exists y', approximated_by y y' /\ ~ y' [= x.
